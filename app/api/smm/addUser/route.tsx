87cbdc2d60b811d2d032afb5870fbb503b5fc2ed
abel <abeloabate01@gmail.com>
2024-11-27 19:27:20 +0300

    DZUNOVBah1

--- app/api/smm/addUser/route.tsx
+++ app/api/smm/addUser/route.tsx
@@ -1,7 +1,6 @@
 import { NextResponse } from "next/server";
 import { Pool } from "pg";
-const { Client } = require('pg-native');
-
+import Client from "pg-native"
 // Set up the connection pool using your connection string
 const pool = new Pool({
     connectionString: "postgresql://postgres.bihqharjyezzxhsghell:newPass12311220yU@aws-0-us-east-1.pooler.supabase.com:6543/postgres?sslmode=require",
