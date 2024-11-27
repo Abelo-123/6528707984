c16d9f7e787a0741ca6e3cb5788de5b2969da99c
abel <abeloabate01@gmail.com>
2024-11-27 19:42:13 +0300

    DZUNOVBah1

--- app/api/smm/addUser/route.tsx
+++ app/api/smm/addUser/route.tsx
@@ -1,13 +1,11 @@
 import { NextResponse } from "next/server";
 import { Pool } from "pg";
-import Client from "pg-native"
 // Set up the connection pool using your connection string
 const pool = new Pool({
     connectionString: "postgresql://postgres.bihqharjyezzxhsghell:newPass12311220yU@aws-0-us-east-1.pooler.supabase.com:6543/postgres?sslmode=require",
     ssl: {
         rejectUnauthorized: false,  // Disable certificate validation
-    },
-    client: new Client()
+    }
 });
 
 export async function POST(req) {
