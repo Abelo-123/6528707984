413b0f8fdd8543a49800566b2ac5117068ff1e8f
abel <abeloabate01@gmail.com>
2024-12-08 21:07:49 +0300

    byPzF9vhRd

--- app/page.tsx
+++ app/page.tsx
@@ -8,10 +8,17 @@ import Smmhistory from './components/Smmhistory/page';
 import Deposit from './components/deposit/page';
 import Account from './components/Account/account';
 import TelegramApp from './components/theme/theme';
+import React, { Suspense } from "react";
+import { useSearchParams } from "next/navigation";
 
 import { useActivePage } from './components/ActivePageContext';
 
+function Content() {
+  const searchParams = useSearchParams();
+  const data = searchParams.get("data"); // Fetch the "data" parameter from the URL
 
+  return <p>Data: {data}</p>; // Display the data
+}
 
 const Telegram = () => {
   const { activePage } = useActivePage();
@@ -48,7 +55,9 @@ const Telegram = () => {
       <AppRoot>
         <div className='w-screen' >
 
-
+          <Suspense fallback={<p>Loading data...</p>}>
+            <Content />
+          </Suspense>
           <div
             id="1"
             className={`w-screen ${activePage === 1 ? '' : 'hidden'}`}><Smm /></div>
