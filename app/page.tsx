fe46fb76c061ee2f061ca0ce281878c4dba78499
abel <abeloabate01@gmail.com>
2024-12-08 20:39:32 +0300

    byPzF9vhRd

--- app/page.tsx
+++ app/page.tsx
@@ -8,17 +8,10 @@ import Smmhistory from './components/Smmhistory/page';
 import Deposit from './components/deposit/page';
 import Account from './components/Account/account';
 import TelegramApp from './components/theme/theme';
-import React, { Suspense } from "react";
-import { useSearchParams } from "next/navigation";
 
 import { useActivePage } from './components/ActivePageContext';
 
-function Content() {
-  const searchParams = useSearchParams();
-  const data = searchParams.get("data"); // Fetch the "data" parameter from the URL
 
-  return <p>Data: {data}</p>; // Display the data
-}
 
 const Telegram = () => {
   const { activePage } = useActivePage();
@@ -52,31 +45,29 @@ const Telegram = () => {
   return (
     <>
 
+      <AppRoot>
+        <div className='w-screen' >
 
-      <div className='w-screen' >
 
-        <Suspense fallback={<p>Loading data...</p>}>
-          <Content />
-        </Suspense>
-        <div
-          id="1"
-          className={`w-screen ${activePage === 1 ? '' : 'hidden'}`}><Smm /></div>
-        <div
-          id="3"
-          className={`w-screen ${activePage === 2 ? '' : 'hidden  '} `}><Smmhistory />
+          <div
+            id="1"
+            className={`w-screen ${activePage === 1 ? '' : 'hidden'}`}><Smm /></div>
+          <div
+            id="3"
+            className={`w-screen ${activePage === 2 ? '' : 'hidden  '} `}><Smmhistory />
+          </div>
+          <div
+            id="4"
+            className={`w-screen ${activePage === 4 ? '' : 'hidden'}`}><Deposit /></div>
+          <div id="5"
+            className={`w-screen ${activePage === 3 ? '' : 'hidden'}`}><Account /></div>
+          <div id="6"
+            className={`w-screen ${activePage === 5 ? '' : 'hidden'}`}><TelegramApp /></div>
+          <div id="7"
+            className={`w-screen ${activePage === 6 ? '' : 'hidden'}`}><Admin /></div>
         </div>
-        <div
-          id="4"
-          className={`w-screen ${activePage === 4 ? '' : 'hidden'}`}><Deposit /></div>
-        <div id="5"
-          className={`w-screen ${activePage === 3 ? '' : 'hidden'}`}><Account /></div>
-        <div id="6"
-          className={`w-screen ${activePage === 5 ? '' : 'hidden'}`}><TelegramApp /></div>
-        <div id="7"
-          className={`w-screen ${activePage === 6 ? '' : 'hidden'}`}><Admin /></div>
-      </div>
 
-
+      </AppRoot >
     </>
   );
 };
