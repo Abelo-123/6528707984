7b89b25c3e36915c9805ca62ea785c87f9cf5def
abel <abeloabate01@gmail.com>
2024-12-25 17:06:09 +0300

    d

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 "use client"
-import { useEffect, useState } from "react";
-import { useUser } from "./components/UserContext";
+// import { useEffect, useState } from "react";
+// import { useUser } from "./components/UserContext";
 import "./globals.css";
 import { ActivePageProvider } from "./components/ActivePageContext"
 import { UserProvider } from "./components/UserContext"
@@ -9,7 +9,7 @@ import Tab from './components/tab/page';
 import Lays from './components/layout/page';
 import { AppRoot } from "@telegram-apps/telegram-ui";
 import { NextTWAProvider } from 'next-twa';
-import { supabase } from "./lib/supabaseClient";
+//import { supabase } from "./lib/supabaseClient";
 
 
 
@@ -22,33 +22,33 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const [normal, isNormal] = useState(null);
-  const [setVersion] = useState(null);
-  const userData = useUser()
-  useEffect(() => {
-    // Load the Telegram Web App JavaScript SDK
-    const script = document.createElement("script");
-    script.src = "https://telegram.org/js/telegram-web-app.js?2";
-    script.async = true;
-    document.body.appendChild(script);
+  // const [normal, isNormal] = useState(null);
+  // const [setVersion] = useState(null);
+  // // const userData = useUser()
+  // useEffect(() => {
+  //   // Load the Telegram Web App JavaScript SDK
+  //   const script = document.createElement("script");
+  //   script.src = "https://telegram.org/js/telegram-web-app.js?2";
+  //   script.async = true;
+  //   document.body.appendChild(script);
 
-    script.onload = () => {
-      const Telegram = window.Telegram;
+  //   script.onload = () => {
+  //     const Telegram = window.Telegram;
 
-      if (window.Telegram && window.Telegram.WebApp) {
-        Telegram.WebApp.expand() // Get the app version
-        const telegramVersion = Telegram.WebApp.version; // Get the app version
-        if (telegramVersion > 7.9) {
-          isNormal(true)
-          setVersion(telegramVersion)
-        } else if (telegramVersion < 8.0) {
-          isNormal(false)
-          setVersion(telegramVersion)
-        }
-      }
+  //     if (window.Telegram && window.Telegram.WebApp) {
+  //       Telegram.WebApp.expand() // Get the app version
+  //       const telegramVersion = Telegram.WebApp.version; // Get the app version
+  //       if (telegramVersion > 7.9) {
+  //         isNormal(true)
+  //         setVersion(telegramVersion)
+  //       } else if (telegramVersion < 8.0) {
+  //         isNormal(false)
+  //         setVersion(telegramVersion)
+  //       }
+  //     }
 
-    };
-  })
+  //   };
+  // })
 
 
   return (
