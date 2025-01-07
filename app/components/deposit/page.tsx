935abe4661f362e5a7eac9c92cbfa46923a837f2
abel <abeloabate01@gmail.com>
2025-01-07 19:14:21 +0300

    popp

--- app/components/deposit/page.tsx
+++ app/components/deposit/page.tsx
@@ -323,15 +323,20 @@ const Deposit = () => {
 
 
                             setaAmount('')
-                            const timestamp = Date.now(); // Current timestamp
+                            const timestamp = Date.now(); // Get the current timestamp
                             const date = new Date(timestamp); // Convert timestamp to a Date object
+
+                            // Extract year, month, and day
+                            const year = date.getFullYear();
+                            const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are zero-based, so add 1
+                            const day = String(date.getDate()).padStart(2, '0');
 
                             setData((prevData) => [
 
                                 {
                                     status: "Done",
                                     did: did,
-                                    date: date.toISOString().split('T')[0],
+                                    date: `${year}-${month}-${day}`,
                                     transaction: mess,
                                     amount: Number(doll),
                                 },
