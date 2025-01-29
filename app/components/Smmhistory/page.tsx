c35ef0c6ceae4e272a4f0dea886f7948b439dbf2
abel <abeloabate01@gmail.com>
2025-01-29 12:47:20 +0300

    d2

--- app/components/Smmhistory/page.tsx
+++ app/components/Smmhistory/page.tsx
@@ -258,7 +258,7 @@ const Smmhistory = () => {
                 <Section header="Order History" style={{ marginTop: '-0.5rem', border: "1px solid var(--tgui--section_bg_color)" }}>
                     <div style={{ width: "100%" }} className=" mx-auto">
                         {loader && <MyLoader />}
-                        <div style={{ borderRadius: "10px", height: '25rem', width: '100%' }} className="scrollabler bg-red-100 w-full overflow-x-auto">
+                        <div style={{ borderRadius: "10px", height: '29rem', width: '100%' }} className="scrollabler bg-red-100 w-full overflow-x-auto">
 
                             {!loader &&
                                 <table style={{ width: "100%" }} className="   rounded-lg shadow-md">
